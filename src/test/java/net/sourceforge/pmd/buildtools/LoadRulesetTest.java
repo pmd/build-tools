23a62410ef4abfc012a1f036e0cad8860a20d71b
Andreas Dangel <andreas.dangel@adangel.org>
2019-03-02 16:43:39 +0100

    Upgrade pmd to 6.12.0

--- src/test/java/net/sourceforge/pmd/buildtools/LoadRulesetTest.java
+++ src/test/java/net/sourceforge/pmd/buildtools/LoadRulesetTest.java
@@ -5,13 +5,20 @@
 package net.sourceforge.pmd.buildtools;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemErrRule;
 
+import net.sourceforge.pmd.RulePriority;
 import net.sourceforge.pmd.RuleSet;
 import net.sourceforge.pmd.RuleSetFactory;
 import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.util.ResourceLoader;
 
 public class LoadRulesetTest {
+
+    @Rule
+    public SystemErrRule systemerr = new SystemErrRule().enableLog();
 
     @Test
     public void testLoadDogFoodRuleset() throws RuleSetNotFoundException {
@@ -24,9 +31,10 @@ public class LoadRulesetTest {
     }
 
     private void assertRuleset(String rulesetName) throws RuleSetNotFoundException {
-        RuleSetFactory ruleSetFactory = new RuleSetFactory();
+        RuleSetFactory ruleSetFactory = new RuleSetFactory(new ResourceLoader(), RulePriority.LOW, true, false);
         RuleSet ruleset = ruleSetFactory.createRuleSet(rulesetName);
         Assert.assertNotNull(ruleset);
         Assert.assertFalse(ruleset.getRules().isEmpty());
+        Assert.assertTrue(systemerr.getLog().isEmpty()); // there should be no deprecation warnings...
     }
 }

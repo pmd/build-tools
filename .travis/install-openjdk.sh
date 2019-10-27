a96e59f50f2908c98ee310196ff2ce6bba6758ec
Andreas Dangel <andreas.dangel@pmd-code.org>
2019-10-27 11:52:29 +0100

    [ci] Fix openjdk version

--- .travis/install-openjdk.sh
+++ .travis/install-openjdk.sh
@@ -1,19 +1,12 @@
 #
 # Original sources:
-# Linux: https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/jdk-11.0.3%2B7/
-#        https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-x64_linux_11.0.3_7.tar.gz
-# MacOSX: https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/jdk-11.0.3%2B7/
-#         https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.tar.gz
+# Linux: https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/jdk-11.0.5%2B10/
+#        https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-x64_linux_11.0.5_10.tar.gz
 #
 
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    OPENJDK_ARCHIVE=OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.tar.gz
-    COMPONENTS_TO_STRIP=3 # e.g. jdk-11.0.3+7/Contents/Home/bin/java
-else
-    OPENJDK_ARCHIVE=OpenJDK11U-x64_linux_11.0.3_7.tar.gz
-    COMPONENTS_TO_STRIP=1 # e.g. openjdk-11.0.3+7/bin/java
-fi
+OPENJDK_ARCHIVE=OpenJDK11U-x64_linux_11.0.5_10.tar.gz
+COMPONENTS_TO_STRIP=1 # e.g. openjdk-11.0.3+7/bin/java
 
 DOWNLOAD_URL=https://pmd-code.org/${OPENJDK_ARCHIVE}
 LOCAL_DIR=${HOME}/.cache/openjdk

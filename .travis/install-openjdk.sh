26202ce3bc11162b279d280c0edce781a1cc853c
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-10-27 15:09:56 +0100

    [ci] Update openjdk11

--- .travis/install-openjdk.sh
+++ .travis/install-openjdk.sh
@@ -1,22 +1,20 @@
 #
-# OpenJDK by AdoptOpenJDK (https://adoptopenjdk.net/)
+# Load OpenJDK by AdoptOpenJDK from https://pmd-code.org/openjdk/latest/jdk-11-linux64.tar.gz
+#
+# Builds are originally from AdoptOpenJDK (https://adoptopenjdk.net/)
 #
 
-# VERSION_TAG e.g. "11.0.4+11" or "13+33"
-VERSION_TAG=$1
-OPENJDK_MAJOR=${VERSION_TAG/.*/}
-OPENJDK_MAJOR=${OPENJDK_MAJOR/+*/}
-#BASE_URL=https://github.com/AdoptOpenJDK/openjdk${OPENJDK_MAJOR}-binaries/releases/download
-BASE_URL=https://pmd-code.org/openjdk
+# OPENJDK_VERSION e.g. "11"
+OPENJDK_VERSION=$1
+BASE_URL=https://pmd-code.org/openjdk/latest/jdk-${OPENJDK_VERSION}
 
-
-DOWNLOAD_URL=${BASE_URL}/jdk-${VERSION_TAG/+/%2B}/OpenJDK${OPENJDK_MAJOR}U-jdk_x64_linux_hotspot_${VERSION_TAG/+/_}.tar.gz
+DOWNLOAD_URL=${BASE_URL}-linux64.tar.gz
 COMPONENTS_TO_STRIP=1 # e.g. openjdk-11.0.3+7/bin/java
 
 OPENJDK_ARCHIVE=$(basename $DOWNLOAD_URL)
 
 LOCAL_DIR=${HOME}/.cache/openjdk
-TARGET_DIR=${HOME}/openjdk11
+TARGET_DIR=${HOME}/openjdk${OPENJDK_VERSION}
 
 mkdir -p ${LOCAL_DIR}
 mkdir -p ${TARGET_DIR}

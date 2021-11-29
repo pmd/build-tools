3e836cd0f5e12bc9c1dbf2b576e0b3ee7422e351
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-11-29 11:24:30 +0100

    Fix deprecations

--- src/test/java/net/sourceforge/pmd/buildtools/LoadRulesetTest.java
+++ src/test/java/net/sourceforge/pmd/buildtools/LoadRulesetTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
@@ -11,9 +11,8 @@ import org.junit.contrib.java.lang.system.SystemErrRule;
 
 import net.sourceforge.pmd.RulePriority;
 import net.sourceforge.pmd.RuleSet;
-import net.sourceforge.pmd.RuleSetFactory;
-import net.sourceforge.pmd.RuleSetNotFoundException;
-import net.sourceforge.pmd.util.ResourceLoader;
+import net.sourceforge.pmd.RuleSetLoadException;
+import net.sourceforge.pmd.RuleSetLoader;
 
 public class LoadRulesetTest {
 
@@ -21,18 +20,21 @@ public class LoadRulesetTest {
     public SystemErrRule systemerr = new SystemErrRule().enableLog();
 
     @Test
-    public void testLoadDogFoodRuleset() throws RuleSetNotFoundException {
+    public void testLoadDogFoodRuleset() throws RuleSetLoadException {
         assertRuleset("net/sourceforge/pmd/pmd-dogfood-config.xml");
     }
 
     @Test
-    public void testLoadUiDogFoodRuleset() throws RuleSetNotFoundException {
+    public void testLoadUiDogFoodRuleset() throws RuleSetLoadException {
         assertRuleset("net/sourceforge/pmd/pmd-ui-dogfood-config.xml");
     }
 
-    private void assertRuleset(String rulesetName) throws RuleSetNotFoundException {
-        RuleSetFactory ruleSetFactory = new RuleSetFactory(new ResourceLoader(), RulePriority.LOW, true, false);
-        RuleSet ruleset = ruleSetFactory.createRuleSet(rulesetName);
+    private void assertRuleset(String rulesetName) throws RuleSetLoadException {
+        RuleSetLoader ruleSetLoader = new RuleSetLoader()
+                .filterAbovePriority(RulePriority.LOW)
+                .warnDeprecated(true)
+                .enableCompatibility(false);
+        RuleSet ruleset = ruleSetLoader.loadFromResource(rulesetName);
         Assert.assertNotNull(ruleset);
         Assert.assertFalse(ruleset.getRules().isEmpty());
         Assert.assertTrue(systemerr.getLog().isEmpty()); // there should be no deprecation warnings...

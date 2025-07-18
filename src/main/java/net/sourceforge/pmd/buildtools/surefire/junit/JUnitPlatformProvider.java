c6dba1b04a20b8d1204f3dcc72c712909fe4fabe
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-07-18 17:27:10 +0200

    chore: apply checkstyle to build-tools as well (#82)
    * chore: apply checkstyle to build-tools as well
    
    * chore: fix checkstyle errors

--- src/main/java/net/sourceforge/pmd/buildtools/surefire/junit/JUnitPlatformProvider.java
+++ src/main/java/net/sourceforge/pmd/buildtools/surefire/junit/JUnitPlatformProvider.java
@@ -1,3 +1,7 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
 package net.sourceforge.pmd.buildtools.surefire.junit;
 
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
--- src/test/java/net/sourceforge/pmd/buildtools/surefire/MockedConsoleLogger.java
+++ src/test/java/net/sourceforge/pmd/buildtools/surefire/MockedConsoleLogger.java
@@ -1,3 +1,7 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
 package net.sourceforge.pmd.buildtools.surefire;
 
 import static org.hamcrest.MatcherAssert.assertThat;
